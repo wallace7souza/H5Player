b87b458939aee88f0dfe0005710ed1b24b205a61
unknown <wallace.c.souza@gmail.com>
2013-12-25 02:16:34 -0200

    Melhorias++

--- html5Player/player.js
+++ html5Player/player.js
@@ -1,31 +1,26 @@
+$(document).ready(function(){
+	playerSettings();
+});
 function playerSettings(){
 	
 	$("#prevBtn").click(function(){
-		if(idxPlaying==0){
-			return;
-		}else{
-			 readMP3(--idxPlaying);
-		}
+		readMP3(--idxPlaying);	  
 	});
 
  $("#nextBtn").click(function(){
- 	if(idxPlaying==playListArray.length){
-			return;
-		}else{
-			 readMP3(++idxPlaying);
-		}
+ 	readMP3(++idxPlaying);
  });
 
 
-$("#playBtn,#pauseBtn").click(function(){
+// $("#playBtn,#pauseBtn").click(function(){
 
-	if($("#playBtn").is(":visible")){
-		$("#player").
-	}else if($("#pauseBtn").is(":visible")){
+// 	if($("#playBtn").is(":visible")){
+// 		$("#player").
+// 	}else if($("#pauseBtn").is(":visible")){
 
-	}
+// 	}
 
-});
+// });
 
 
 
